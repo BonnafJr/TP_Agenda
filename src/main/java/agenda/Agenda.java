53f331df580117c42de905aa542a87c55e3a32a1
paul <paulortega@laposte.net>
2021-12-13 16:21:53 +0100

    Paul Agenda

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -3,32 +3,39 @@ package agenda;
 import java.time.LocalDate;
 import java.util.*;
 
-/**
- * Description : An agenda that stores events
- */
 public class Agenda {
-    
-ArrayList<Event> eventList;
+
+    private final List<Event> eventList;
 
     public Agenda() {
-        this.eventList = new ArrayList<>();
+        eventList = new ArrayList<>();
     }
 
     public void addEvent(Event e) {
         eventList.add(e);
     }
 
-    /**
-     * Computes the events that occur on a given day
-     *
-     * @param day the day toi test
-     * @return and iteraror to the events that occur on that day
-     */
+ 
     public List<Event> eventsInDay(LocalDate day) {
-        for (int i = 0; i < eventList.size(); i++) {
-            if(eventList.get(i).getStart() == day){
-                return eventList.get(i);
+        ArrayList<Event> eventsDay = new ArrayList<>();
+        for (Event e : eventList) {
+            if (e.isInDay(day)) {
+                eventsDay.add(e);
             }
         }
-}
+        return eventsDay;
+    }
+
+    public List<Event> findByTitle(String title) {
+        ArrayList<Event> eventsDay = new ArrayList<>();
+        eventList.stream().filter(e -> (e.getTitle().equals(title))).forEachOrdered(e -> {
+            eventsDay.add(e);
+        });
+        return eventsDay;
+    }
+
+    public boolean isFreeFor(Event e) {
+        return eventList.stream().noneMatch(event -> (e.getStart().plus(e.getDuration()).isAfter(event.getStart()) && e.getStart().isBefore(event.getStart().plus(event.getDuration()))));
+    }
+
 }
